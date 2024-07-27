2a14530cf482d1cd3e99eb065b5f733754c56e0f
zeeshan4859 <zeeshan4859@gmail.com>
2024-07-27 22:12:44 +0500

    i have made change in Appjs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello From ok ok GT</h1>
+        <h1>Hello Zeeshan Ali</h1>
       </header>
     </div>
   );
