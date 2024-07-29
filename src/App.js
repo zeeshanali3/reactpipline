6f49db192dc53ec28c3b49d647d67cf94700f922
zeeshan4859 <zeeshan4859@gmail.com>
2024-07-29 12:39:55 +0500

    update in code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Zeeshan Ali</h1>
+        <h1>Hello Pipline Project</h1>
       </header>
     </div>
   );
